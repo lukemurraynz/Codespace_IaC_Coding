d0f5ac63edffdfa2f8c74972a7cf1365f1014c9a
Luke Murray <24467442+lukemurraynz@users.noreply.github.com>
2025-01-07 07:53:38 +0000

    commit message

--- example.tf
+++ example.tf
@@ -1,10 +1,6 @@
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
-  tags = {
-    yor_name  = "example"
-    yor_trace = "f9bc7151-2e2d-4ded-9385-613ec6adcf3c"
-  }
 }
 
 resource "azurerm_storage_account" "example" {
@@ -16,7 +12,5 @@ resource "azurerm_storage_account" "example" {
 
   tags = {
     environment = "staging"
-    yor_name    = "example"
-    yor_trace   = "e1439513-b7ed-477b-88c1-cac4ffec5dd6"
   }
 }
